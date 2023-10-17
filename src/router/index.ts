2fb3a4e40abffeff48494e29fc1b11905c2ad395
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2023-10-17 19:23:23 -0300

    feat: singup page (deus)

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,12 @@ import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue'
 import LoginPage from '@/views/LoginPage.vue';
+import SingupPage from '@/views/SingupPage.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/login'
+    redirect: '/register'
   },
   {
     path: '/home',
@@ -17,7 +18,12 @@ const routes: Array<RouteRecordRaw> = [
     path: '/login',
     name: 'Login',
     component: LoginPage
-  }
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: SingupPage
+  },
 ]
 
 const router = createRouter({
