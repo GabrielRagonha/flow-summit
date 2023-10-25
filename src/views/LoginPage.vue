49d8774939be88b1615d1379c9aa8f748a3844ca
Gabriel Rodrigues <gabrielragonhaprofissional@outlook.com>
2023-10-25 13:04:53 -0300

    chore: use AuthenticateTop component

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,6 +1,5 @@
 <template>
   <ion-page>
-
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
@@ -8,40 +7,57 @@
         </ion-toolbar>
       </ion-header>
 
-      <div class="h-full w-full flex flex-col py-10 px-4 text-center gap-8">
-
-        <div class="flex flex-col gap-2 mb-10">
-          <h1 class="text-3xl font-bold">FlowSummit</h1>
-          <p></p>
+      <div
+        class="h-full w-full flex flex-col py-10 px-4 text-center gap-8 justify-start"
+      >
+        <div class="flex flex-col gap-8">
+          <AuthenticateTop
+            :title="'Login'"
+            :desc="'Entre agora e garanta acesso aos melhores eventos da região!'"
+          />
         </div>
 
         <div class="flex flex-col gap-4">
           <ion-item>
             <ion-label>Email</ion-label>
-            <ion-input type="password" placeholder="No mínimo 8 caracteres" :minlength="8" :required="true" />
+            <ion-input
+              type="password"
+              placeholder="No mínimo 8 caracteres"
+              :minlength="8"
+              :required="true"
+            />
           </ion-item>
 
           <ion-item>
             <ion-label>Senha</ion-label>
-            <ion-input type="password" placeholder="No mínimo 8 caracteres" :minlength="8" :required="true" />
+            <ion-input
+              type="password"
+              placeholder="No mínimo 8 caracteres"
+              :minlength="8"
+              :required="true"
+            />
           </ion-item>
-
         </div>
 
         <div class="flex flex-col gap-6">
-          <ion-button class="w-full h-14">Entrar</ion-button>
-          <p></p>
+          <ion-button fill="outline" color="dark" class="w-full h-14">Entrar</ion-button>
         </div>
-
       </div>
-
     </ion-content>
-
-
   </ion-page>
 </template>
-  
+
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonButton, IonLabel } from '@ionic/vue';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonInput,
+  IonButton,
+  IonLabel,
+} from "@ionic/vue";
+
+import AuthenticateTop from "../components/AuthenticateTop.vue";
 </script>
-    
--- src/views/SignupPage.vue
+++ src/views/SignupPage.vue
@@ -1,6 +1,5 @@
 <template>
   <ion-page>
-
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
@@ -8,46 +7,68 @@
         </ion-toolbar>
       </ion-header>
 
-      <div class="h-full w-full flex flex-col py-10 px-4 text-center gap-8">
-
-        <div class="flex flex-col gap-2 mb-10">
-          <h1 class="text-3xl font-bold">FlowSummit</h1>
-          <p></p>
+      <div
+        class="h-full w-full flex flex-col py-10 px-4 text-center gap-8 justify-start"
+      >
+        <div class="flex flex-col gap-8">
+          <AuthenticateTop
+            :title="'Crie sua conta'"
+            :desc="'Faça agora seu registro e conheça o FlowSummit!'"
+          />
         </div>
 
         <div class="flex flex-col gap-4">
-
           <ion-item>
             <ion-label>Nome</ion-label>
-            <ion-input type="text" placeholder="No mínimo 8 caracteres" :minlength="8" :required="true" />
+            <ion-input
+              type="text"
+              placeholder="No mínimo 8 caracteres"
+              :minlength="8"
+              :required="true"
+            />
           </ion-item>
 
           <ion-item>
             <ion-label>Email</ion-label>
-            <ion-input type="password" placeholder="No mínimo 8 caracteres" :minlength="8" :required="true" />
+            <ion-input
+              type="password"
+              placeholder="No mínimo 8 caracteres"
+              :minlength="8"
+              :required="true"
+            />
           </ion-item>
 
           <ion-item>
             <ion-label>Senha</ion-label>
-            <ion-input type="password" placeholder="No mínimo 8 caracteres" :minlength="8" :required="true" />
+            <ion-input
+              type="password"
+              placeholder="No mínimo 8 caracteres"
+              :minlength="8"
+              :required="true"
+            />
           </ion-item>
-
         </div>
 
         <div class="flex flex-col gap-6">
-          <ion-button class="w-full h-14">Registrar</ion-button>
+          <ion-button fill="outline" color="dark" class="w-full h-14">Registrar</ion-button>
           <p></p>
         </div>
-
       </div>
-
     </ion-content>
-
-
   </ion-page>
 </template>
-  
+
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonButton, IonLabel } from '@ionic/vue';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonInput,
+  IonButton,
+  IonLabel,
+} from "@ionic/vue";
+
+import AuthenticateTop from "../components/AuthenticateTop.vue";
 </script>
-    
