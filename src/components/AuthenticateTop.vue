60ec918eb4ce46f0967f555ba32e2829cdc516a6
Gabriel Rodrigues <gabrielragonhaprofissional@outlook.com>
2023-10-25 13:02:47 -0300

    feat: create AuthenticateTop component

---
+++ src/components/AuthenticateTop.vue
@@ -0,0 +1,21 @@
+<template>
+  <ion-img
+    src="/icons/logo.svg"
+    class="max-w-[70%] md:max-w-lg m-auto"
+    alt="FlowSummit logo"
+  ></ion-img>
+
+  <p class="flex items-center justify-center flex-col gap-2">
+    <span class="text-3xl font-bold">{{ title }}</span>
+    <span class="text-sm">{{ desc }}</span>
+  </p>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    title: String,
+    desc: String,
+  },
+};
+</script>
