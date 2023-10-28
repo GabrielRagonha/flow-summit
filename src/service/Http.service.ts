dded8805136504b673859533823da193ded08d22
Gabriel Rodrigues <gabrielragonhaprofissional@outlook.com>
2023-10-27 21:32:25 -0300

    feat: create Http service

---
+++ src/service/Http.service.ts
@@ -0,0 +1,18 @@
+export class HttpService {
+  private baseUrl = "http://localhost:3000/"
+  public async post<T>(url: string, body: any): Promise<{ status: number, data: T }> {
+    const rawResponse = await fetch(this.baseUrl + url, {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+
+    const { status } = rawResponse
+
+    const response = await rawResponse.json()
+
+    return { status, data: response };
+  }
+}
