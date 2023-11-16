0c846b000cf57db6b662a19e709451b209a1abd2
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2023-11-16 19:50:41 -0300

    feat: create project response and service

---
+++ src/interfaces/ProjectRespose.d.ts
@@ -0,0 +1,10 @@
+export interface ProjectResponse {
+  idEvent: string,
+  name: string;
+  description: string;
+  startDate: Date;
+  endDate: Date;
+  idCoordinator: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
