9da86e73d15c1daecc75684f79d0ee2c27d51816
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2023-11-17 01:47:32 -0300

    feat: add cards props from API

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,13 @@
 <template>
   <section>
     <SubHeader />
-  
+
     <ion-page id="main-content">
       <Header />
-  
+
       <ion-content :fullscreen="true">
         <div id="container">
-          <CardList />
+          <CardList :projects="projects" />
         </div>
       </ion-content>
     </ion-page>
@@ -16,8 +16,20 @@
 
 <script setup lang="ts">
 import { IonContent, IonPage } from "@ionic/vue";
-
+import { ref, onMounted } from "vue";
+import { getProjects } from "@/service/Project.service";
 import Header from "../components/Header.vue";
 import SubHeader from "../components/SubHeader.vue";
 import CardList from "../components/CardList.vue";
+
+const projects = ref<any>([]);
+
+onMounted(async () => {
+  try {
+    const response = await getProjects();
+    projects.value = response?.data;
+  } catch (error) {
+    console.error("Erro:", error);
+  }
+});
 </script>
