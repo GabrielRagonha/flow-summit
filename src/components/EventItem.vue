de5adc5fe0c927efcd5e0901448d072e4fbd55d8
Luis Modesto <luisfelipetecno@gmail.com>
2023-11-17 03:05:21 -0300

    feat: event item component

---
+++ src/components/EventItem.vue
@@ -0,0 +1,13 @@
+<template>
+    <ion-item>
+        <ion-label>{{ info }}</ion-label>
+    </ion-item>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        info: String,
+    },
+};
+</script>
