8764385da04f8f5ffdfdbaa36e271de9cea9c3de
Gabriel Rodrigues <gabrielragonhaprofissional@outlook.com>
2023-11-17 21:50:23 -0300

    feat: add user storage

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,48 +1,26 @@
 <template>
   <ion-page>
     <ion-content :fullscreen="true">
-      <form
-        class="h-full w-full flex flex-col py-10 px-4 text-center gap-8 justify-start"
-        @submit.prevent="handleLogin()"
-      >
+      <form class="h-full w-full flex flex-col py-10 px-4 text-center gap-8 justify-start"
+        @submit.prevent="handleLogin()">
         <div class="flex flex-col gap-8">
-          <AuthenticateTop
-            :title="'Login'"
-            :desc="'Entre agora e garanta acesso aos melhores eventos da região!'"
-          />
+          <AuthenticateTop :title="'Login'" :desc="'Entre agora e garanta acesso aos melhores eventos da região!'" />
         </div>
 
         <div class="flex flex-col gap-4">
           <ion-item>
-            <ion-input
-              type="email"
-              label="Email"
-              placeholder="No mínimo 8 caracteres"
-              :required="true"
-              v-model="email"
-            />
+            <ion-input type="email" label="Email" placeholder="No mínimo 8 caracteres" :required="true" v-model="email" />
           </ion-item>
 
           <ion-item>
-            <ion-input
-              type="password"
-              label="Senha"
-              placeholder="No mínimo 8 caracteres"
-              :required="true"
-              v-model="pass"
-            />
+            <ion-input type="password" label="Senha" placeholder="No mínimo 8 caracteres" :required="true"
+              v-model="pass" />
           </ion-item>
         </div>
 
         <div class="flex flex-col gap-4">
-          <ion-button
-            fill="outline"
-            color="dark"
-            class="w-full h-14"
-            type="submit"
-            >Entrar</ion-button
-          >
-          <p class="mb-5">Não possui um cadastro? 
+          <ion-button fill="outline" color="dark" class="w-full h-14" type="submit">Entrar</ion-button>
+          <p class="mb-5">Não possui um cadastro?
             <a href="/cadastro" class="underline">Faça agora!</a>
           </p>
         </div>
@@ -54,12 +32,9 @@
 <script setup lang="ts">
 import {
   IonContent,
-  IonHeader,
   IonItem,
   IonButton,
   IonPage,
-  IonTitle,
-  IonToolbar,
   IonInput,
 } from "@ionic/vue";
 
@@ -85,6 +60,14 @@ async function handleLogin() {
       "user/login",
       formData
     );
+
+    const { user, token } = result.data
+
+    localStorage.user = JSON.stringify({
+      ...user,
+      password: null
+    })
+    localStorage.token = token
 
     result.status === 200 && router.push("/home");
   } catch (error: any) {
