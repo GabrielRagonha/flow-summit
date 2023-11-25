151fbdbc1eab7f10ea52a8454b0bdb26e9f4dcb0
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2023-11-25 01:49:47 -0300

    feat: add menu buttons

--- src/components/SubHeader.vue
+++ src/components/SubHeader.vue
@@ -5,23 +5,37 @@
         <ion-title>Menu</ion-title>
       </ion-toolbar>
     </ion-header>
+
+    <ion-content class="ion-padding">
+      <div class="flex flex-col gap-2">
+        <a
+          href="/novo-evento"
+          class="bg-[#F97316] shadow p-2 rounded-md flex items-center justify-center w-full text-white active:bg-[#f97316e6] hover:bg-[#f97316e6] transition-all"
+          >Criar novo evento</a
+        >
+
+        <a
+          href="#"
+          class="bg-[#F97316] shadow p-2 rounded-md flex items-center justify-center w-full text-white active:bg-[#f97316e6] hover:bg-[#f97316e6] transition-all"
+          >Ver perfil</a
+        >
+
+        <button
+          class="bg-[#cf1d1d] shadow p-2 rounded-md flex items-center justify-center w-full text-white active:bg-[#cf1d1de6] hover:bg-[#cf1d1de6] transition-all"
+        >
+          Logout
+        </button>
+      </div>
+    </ion-content>
   </ion-menu>
 </template>
 
-<script lang="ts">
-  import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-  import { defineComponent } from 'vue';
-
-  export default defineComponent({
-    components: {
-      IonButtons,
-      IonContent, 
-      IonHeader,
-      IonMenu,
-      IonMenuButton,
-      IonPage,
-      IonTitle,
-      IonToolbar,
-    },
-  });
+<script setup lang="ts">
+import {
+  IonContent,
+  IonHeader,
+  IonMenu,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
 </script>
