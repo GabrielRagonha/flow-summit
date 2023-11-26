994ec92d95ce2c92e7a20a4d101c19a4c58b2b39
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2023-11-26 17:02:51 -0300

    style: adjust style

--- src/views/EventPage.vue
+++ src/views/EventPage.vue
@@ -7,8 +7,7 @@
 
       <ion-content :fullscreen="true">
         <div id="container" class="h-full">
-          <div class="h-full  w-full px-4 flex flex-col justify-between">
-
+          <div class="h-full w-full px-4 flex flex-col justify-between">
             <div>
               <ion-card class="my-2 mx-0">
                 <ion-card-header>
@@ -17,23 +16,31 @@
                 </ion-card-header>
 
                 <ion-card-content>
-                  <img src="https://picsum.photos/1000" v-bind:alt="'Imagem do evento ' + event?.name" />
+                  <img
+                    src="https://picsum.photos/1000"
+                    v-bind:alt="'Imagem do evento ' + event?.name"
+                  />
                 </ion-card-content>
-                <ion-item class="text-sm py-5">
+                
+                <div class="text-sm p-5 text-black">
                   {{ event?.description }}
-                </ion-item>
+                </div>
               </ion-card>
             </div>
 
-            <div class="mb-5">
-              <button id="confirm-btn" @click.prevent="handleConfirm()" fill="outline" color="dark"
-                class="w-full text-white h-14 font-bold bg-[#F97316] rounded-lg" type="submit">
+            <div class="py-5">
+              <button
+                id="confirm-btn"
+                @click.prevent="handleConfirm()"
+                fill="outline"
+                color="dark"
+                class="w-full text-white h-14 font-bold bg-[#F97316] rounded-lg"
+                type="submit"
+              >
                 Confirmar presença
               </button>
             </div>
-
           </div>
-
         </div>
       </ion-content>
     </ion-page>
@@ -69,12 +76,13 @@ onMounted(async () => {
     const scheduleUser = await getSchedule();
     event.value = eventData?.data;
 
-    if (scheduleUser.find(schedule => schedule.idEvent === event.value.idEvent)) {
+    if (
+      scheduleUser.find((schedule) => schedule.idEvent === event.value.idEvent)
+    ) {
       const btn = document.getElementById("confirm-btn");
       btn.innerHTML = "Presença confirmada";
       btn.style.backgroundColor = "#008000";
     }
-
   } catch (error) {
     console.error("Erro:", error);
   }
@@ -85,17 +93,15 @@ async function handleConfirm() {
     const response = await confirmEvent({
       idEvent: router.path.split("/")[2],
       idUser: user.idUser,
-    })
+    });
 
     if (response?.status === 200) {
       const btn = document.getElementById("confirm-btn");
       btn.innerHTML = "Presença confirmada";
       btn.style.backgroundColor = "#008000";
     }
-
   } catch (error) {
     console.error("Erro:", error);
   }
 }
-
 </script>
